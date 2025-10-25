cf9966b2becca78b53ec3b064a8cdb3f2c8dbd28
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-10-25 22:07:29 +0300

    Grade task completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What percentage did you get on the test? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.Write($"Your grade is {letter}.");
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.Write("You have passed!");
+        }
+        else
+        {
+            Console.Write("You have not passed.");
+        }
+
+
+
+
+
+
     }
 }
