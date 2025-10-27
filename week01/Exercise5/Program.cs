cd5d6860ed110e9bd2ee53bf3bc786e62094b51f
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-10-27 22:01:36 +0200

    Function task completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squared = SquareNumber(userNumber);
+        DisplayResults(userName, squared);
+
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favourite number: ");
+        string number = Console.ReadLine();
+        int userNumber = int.Parse(number);
+        return userNumber;
+    }
+    static int SquareNumber(int userNumber)
+    {
+        int squared = userNumber * userNumber;
+        return squared;
+    }
+    static void DisplayResults(string userName, int squared)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squared}");
+    }
+
 }
