135158424994c9b58d38f823d88a80250d537ec9
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-10-27 07:36:14 +0200

    Guessing task completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            string reply = Console.ReadLine();
+            guess = int.Parse(reply);
+
+
+            if (guess < magic_number)
+            {
+                Console.Write("Higher. ");
+            }
+            else if (guess > magic_number)
+            {
+                Console.Write("Lower. ");
+            }
+            else
+            {
+                Console.Write("You have guessed it!");
+            }
+        }
+
+
     }
 }
