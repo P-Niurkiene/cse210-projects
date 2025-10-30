e017f82b7deb03569a63860ed0a6ee5bbbbc09a5
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-10-30 21:57:44 +0200

    Start of journal project

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to your Personal Journal!");
+        Console.WriteLine("Please select a number of what you would like to do:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("Which would you like to do? ");
+        string choice = Console.ReadLine();
+        while (choice != "5")
+        {
+
+        }
+
     }
 }
