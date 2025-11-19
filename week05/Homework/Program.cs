abd046bb7eb1b289eef7edd600faf2134c372009
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-11-19 20:09:16 +0200

    Program updates

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
-class Program
+public partial class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Maths mathsAssignment = new Maths("Sam Biggs", "Fractions", "7.3", "1-15");
+        Console.WriteLine(mathsAssignment.GetSummary());
+        Console.WriteLine(mathsAssignment.GetHomeworkList());
+
+        Console.WriteLine("");
+
+        Writing writingAssignment = new Writing("Sarah Williams", "WW2", "In the Camps");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
