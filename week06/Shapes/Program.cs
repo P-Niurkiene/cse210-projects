10b3b7977dc3dd245f5fdb362243b4673d0b6f94
P-Niurkiene <p.niurkiene@byupathway.edu>
2025-11-27 21:14:23 +0200

    shapes program and classes made

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("Hello This is the Shapes project for Polymorphism");
+
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Pink", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 5, 7);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Orange", 4);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string colour = s.GetColour();
+            double area = s.GetArea();
+            Console.WriteLine($"For the colour {colour} shape the area is {area}");
+        }
+
     }
 }
---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,16 @@
+using System.Formats.Asn1;
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string colour, double side) : base(colour)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
